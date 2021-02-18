963a92a53f90658b3ca7c36c23e4a3ce369bca49
Nick Jacks <njj17@miami.edu>
2021-02-18 11:57:02 -0500

    Created special TriStateBuffer

---
+++ Tri_State_Buffer_Special_16.vhd
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity TriStateBufferSpecial16 is
+    Port ( en : in  STD_LOGIC;
+           input : in  STD_LOGIC_VECTOR (7 downto 0);
+           output : out STD_LOGIC_VECTOR (15 downto 0));
+end TriStateBufferSpecial16;
+
+architectural Behavioral of TriStateBufferSpecial16 is
+  output <= ("00000000" & input) when (en = '1') else "ZZZZZZZZZZZZZZZZ";
+end Behavioral;
