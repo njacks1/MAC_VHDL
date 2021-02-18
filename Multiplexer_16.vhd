8462e003cc30c06021f71c13a5acd53b3edf26ef
Nick Jacks <njj17@miami.edu>
2021-02-18 11:56:33 -0500

    Entity and Architecture name update

--- Multiplexer_16.vhd
+++ Multiplexer_16.vhd
@@ -1,14 +1,14 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity mux_2to1_16 is
+entity Multiplexer_2to1_16 is
     Port ( SEL : in  STD_LOGIC;
            A   : in  STD_LOGIC_VECTOR (15 downto 0);
            B   : in  STD_LOGIC_VECTOR (15 downto 0);
            X   : out STD_LOGIC_VECTOR (15 downto 0));
-end mux_2to1_16;
+end Multiplexer_2to1_16;
 
-architecture Behavioral of mux_2to1_16 is
+architecture Behavioral of Multiplexer_2to1_16 is
 begin
     X <= A when (SEL = '1') else B;
 end Behavioral;
--- Register_8.vhd
+++ Register_8.vhd
@@ -1,15 +1,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity Register_8 is
+entity Register8 is
   Port(
     clk : in STD_LOGIC;
     load : in STD_LOGIC;
     input : in STD_LOGIC_VECTOR(7 downto 0);
     output : in STD_LOGIC_VECTOR(7 downto 0));
-end Register_8;
+end Register8;
 
-architecture Behavioral of Register_8 is
+architecture Behavioral of Register8 is
 Signal storage : STD_LOGIC_VECTOR(7 downto 0);
 begin
   process(clk, input, load)
